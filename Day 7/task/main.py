edbae419ac02a096ed1cdec7c8dd281bf852c142
mrsneaky29 <84763159+mrsneaky29@users.noreply.github.com>
2026-05-27 12:52:33 +0530

    Update main.py

--- Day 7/task/main.py
+++ Day 7/task/main.py
@@ -5,13 +5,10 @@ import hangman_art
 word_list = hangman_words.word_list
 stages = hangman_art.stages
 
-# TODO-1: - Update the word list to use the 'word_list' from hangman_words.py  Done
-
 lives = 6
 
 print(hangman_art.logo)
 
-# TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.  Done
 
 chosen_word = random.choice(word_list)
 print(chosen_word)
@@ -27,12 +24,11 @@ correct_letters = []
 
 while not game_over:
 
-    # TODO-6: - Update the code below to tell the user how many lives they have left.  Done
     print(f"****************************<{lives}>/6 LIVES LEFT****************************")
     guess = input("Guess a letter: ").lower()
     guessed_letters = []
     guessed_letters.append(guess)
-    # TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.  Done
+    
     if guess in guessed_letters:
         print("You already guessed that letter." + guess)
         if guess in correct_letters:
@@ -51,21 +47,15 @@ while not game_over:
 
     print("Word to guess: " + display)
 
-    # TODO-5: - If the letter is not in the chosen_word, print out the letter and let them know it's not in the word.  Done
-    #  e.g. You guessed d, that's not in the word. You lose a life.
-
     if guess not in chosen_word:
         lives -= 1
         print("You guessed " + guess + " It was wrong and you lose a life.")
 
         if lives == 0:
             game_over = True
-
-            # TODO 7: - Update the print statement below to give the user the correct word they were trying to guess.  Done
             print(f"*********************** The word was {chosen_word}! YOU LOSE**********************")
     if "_" not in display:
         game_over = True
         print("****************************YOU WIN****************************")
 
-    # TODO-2: - Update the code below to use the stages List from the file hangman_art.py  Done
     print(stages[lives])
